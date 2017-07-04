4dd91a2365a97bf005ecd09c1fe7cc3669a3c1bc
Nemanja Vasic <nemanjavasa@gmail.com>
2017-07-04 17:08:24 +0200

    Added testGetCurrentTotalForRoom method

--- ProjectHotelManagementTest/projectHotelManagement/test/dao/HotelStatusDAOImplTest.java
+++ ProjectHotelManagementTest/projectHotelManagement/test/dao/HotelStatusDAOImplTest.java
@@ -1,7 +1,6 @@
 package projectHotelManagement.test.dao;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -24,14 +23,15 @@ import projectHotelManagement.data.Room;
 import projectHotelManagement.data.User;
 
 public class HotelStatusDAOImplTest {
-	
+
 	private static final int SERVICE_ID = 0;
 	private static final int ROOM_PRICE = 10;
 	private static final int ROOM_ID = 1;
 	private static final int IS_LOGGED = 1;
 	private static final int USER_AGE = 52;
 	private static final int USER_ID = 1;
-	
+	private static final double DELTA = 1e-15;
+
 	@Mock
 	private Connection connection;
 	@Mock
@@ -51,37 +51,60 @@ public class HotelStatusDAOImplTest {
 		Room room = new Room(ROOM_ID, "Jednosobna", ROOM_PRICE);
 		AdditionalServices additionalService = new AdditionalServices();
 		additionalService.setServicesId(SERVICE_ID);
-		
+
 		HotelStatus hotel = new HotelStatus(user.getPersonalIdNumber(), additionalService.getServicesId(),
 				room.getRoomNumber(), new Date());
-		
+
 		assertNotNull(user);
 		assertNotNull(room);
 		assertNotNull(additionalService);
 		assertNotNull(hotel);
 		assertEquals("Pero", user.getFirstName());
 		assertEquals("Jednosobna", room.getRoomType());
-		
+
 		ResultSet rs = Mockito.mock(ResultSet.class);
 		Mockito.when(rs.getInt(USER_ID)).thenReturn(hotel.getUserId());
 		Mockito.when(rs.getInt(SERVICE_ID)).thenReturn(hotel.getServiceId());
 		Mockito.when(rs.getInt(ROOM_ID)).thenReturn(hotel.getRoomId());
 		Mockito.when(rs.getDate("checkIn")).thenReturn(new java.sql.Date(hotel.getCheckIn().getTime()));
 	}
-	
+
 	@Test
 	public void testBookAdditionalServices() throws SQLException {
 		ArrayList<Integer> selectedServices = new ArrayList<>();
 		selectedServices.add(SERVICE_ID);
-		
+
 		assertNotNull(selectedServices);
 		assertEquals(1, selectedServices.size());
-		
+
 		hotelStatus.bookAdditionalServices(USER_ID, selectedServices);
 		Mockito.verify(hotelStatus, Mockito.times(1)).bookAdditionalServices(USER_ID, selectedServices);
 	}
-	
-	
+
+	@Test
+	public void testGetCurrentTotalForServices() {
+		AdditionalServices additionalServiceGym = new AdditionalServices();
+		additionalServiceGym.setServicesId(SERVICE_ID);
+		int daysStayed = 2;
+		int totalToPay = 0;
+
+		totalToPay = daysStayed * additionalServiceGym.getGymPrice();
+		assertNotNull(additionalServiceGym);
+		assertEquals(SERVICE_ID, additionalServiceGym.getServicesId());
+		assertEquals(20, totalToPay);
+	}
+
+	@Test
+	public void testGetCurrentTotalForRoom() {
+		Room apartment = new Room(ROOM_ID, "Apartment", 80);
+		int daysStayed = 2;
+		double totalForRoomServices = 0;
+
+		totalForRoomServices = daysStayed * apartment.getPrice();
+		assertEquals(160, totalForRoomServices, DELTA);
+		assertNotNull(apartment);
+		assertEquals(ROOM_ID, apartment.getRoomNumber());
+	}
 
 	@After
 	public void tearDown() {
