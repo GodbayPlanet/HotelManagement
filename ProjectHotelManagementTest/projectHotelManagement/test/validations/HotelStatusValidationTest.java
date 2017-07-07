fd833414e03f2b32cae4822e5de0894906255d6c
Nemanja Vasic <nemanjavasa@gmail.com>
2017-07-07 18:08:19 +0200

    Added testIsRoomExists

--- ProjectHotelManagementTest/projectHotelManagement/test/validations/HotelStatusValidationTest.java
+++ ProjectHotelManagementTest/projectHotelManagement/test/validations/HotelStatusValidationTest.java
@@ -13,18 +13,24 @@ import static org.junit.Assert.*;
 
 import projectHotelManagement.connection.DBConnection;
 import projectHotelManagement.dao.UserDAOImpl;
+import projectHotelManagement.data.Room;
 import projectHotelManagement.data.User;
 import projectHotelManagement.validations.HotelStatusValidation;
 
 public class HotelStatusValidationTest {
 
+	private static final int OFF_LINE = 0;
+	private static final int USER_AGE = 26;
+	private static final int ROOM_PRICE = 50;
+	private static final int ROOM_ID = 1;
 	private static final int USER_ID = 1;
 	@Mock
 	private Connection mockConnection;
 	@Mock
 	private UserDAOImpl mockCustomer;
 	private HotelStatusValidation validation;
-	private User user = new User(USER_ID, "Perica", "Pero", "Peric", "pass", "M", 26, 0);
+	private User user = new User(USER_ID, "Perica", "Pero", "Peric", "pass", "M", USER_AGE, OFF_LINE);
+	private Room room = new Room(ROOM_ID, "SingleRoom", ROOM_PRICE);
 
 	@Before
 	public void setUp() {
@@ -41,14 +47,21 @@ public class HotelStatusValidationTest {
 
 	@Test
 	public void testIsUserNameExists() throws SQLException {
-		assertTrue(HotelStatusValidation.isUserNameExists("Perica"));
+		assertTrue(HotelStatusValidation.isUserNameExists(user.getUserName()));
 	}
 	
 	@Test
 	public void testIsUserFirstNameExists() throws SQLException {
-		assertTrue(HotelStatusValidation.isUserFirstNameExists("Pero"));
+		assertTrue(HotelStatusValidation.isUserFirstNameExists(user.getFirstName()));
 	}
-
+	
+	@Test
+	public void testIsRoomExists() {
+		assertTrue(HotelStatusValidation.isRoomExists(room.getRoomNumber()));
+	}
+	
+	
+	
 	@After
 	public void tearDown() {
 		try {
