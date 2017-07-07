059803d8fc9c373e0ca52f4d672e6963ef968a96
Nemanja Vasic <nemanjavasa@gmail.com>
2017-07-07 17:33:29 +0200

    Added testIsUserNameExists

--- ProjectHotelManagementTest/projectHotelManagement/test/validations/HotelStatusValidationTest.java
+++ ProjectHotelManagementTest/projectHotelManagement/test/validations/HotelStatusValidationTest.java
@@ -13,30 +13,36 @@ import org.mockito.MockitoAnnotations;
 import projectHotelManagement.connection.DBConnection;
 import projectHotelManagement.dao.UserDAOImpl;
 import projectHotelManagement.validations.HotelStatusValidation;
-import static org.junit.Assert.*;
 
 public class HotelStatusValidationTest {
-	
+
 	private static final int USER_ID = 1;
 	@Mock
 	private Connection mockConnection;
 	@Mock
 	private UserDAOImpl mockCustomer;
-	
+	private HotelStatusValidation validation;
+
 	@Before
 	public void setUp() {
 		mockConnection = DBConnection.getConnectionToDB();
 		mockCustomer = Mockito.mock(UserDAOImpl.class);
+		validation = Mockito.spy(new HotelStatusValidation());
 		MockitoAnnotations.initMocks(this);
 	}
-	
+
 	@Test
 	public void testIsUserIdExists() {
-		final HotelStatusValidation validation = Mockito.spy(new HotelStatusValidation());
 		Mockito.verify(validation);
 		HotelStatusValidation.isUserIdExists(USER_ID);
 	}
-	
+
+	@Test
+	public void testIsUserNameExists() throws SQLException {
+		Mockito.verify(validation);
+		HotelStatusValidation.isUserNameExists("Perica");
+	}
+
 	@After
 	public void tearDown() {
 		try {
