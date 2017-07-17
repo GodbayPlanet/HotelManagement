1e9e02cda155807c14d719e7b11563e3d2f286e7
Nemanja Vasic <nemanjavasa@gmail.com>
2017-07-17 15:21:01 +0200

    Added testUpdateRoomEndDate and testUpdateServiceEndDate methods

--- projectHotelManagementDBUnitTest/projectHotelManagement/dbunit/testDao/TestHotelStatusDAOImpl.java
+++ projectHotelManagementDBUnitTest/projectHotelManagement/dbunit/testDao/TestHotelStatusDAOImpl.java
@@ -5,12 +5,13 @@ import static org.junit.Assert.assertNotNull;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 import org.dbunit.dataset.DataSetException;
 import org.dbunit.dataset.IDataSet;
-import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,14 +20,16 @@ import projectHotelManagement.dao.HotelStatusDAOImpl;
 
 public class TestHotelStatusDAOImpl {
 
-	private static final int EXPECTED_AMOUNT_FOR_ROOM = 40;
-	private static final int EXPECTED_AMOUNT_FOR_SERVICES = 20;
+	private static final int SECOND_ROOM_ID = 1;
+	private static final int SECOND_CUSTOMER_ID = 1;
+	private static final int EXPECTED_AMOUNT_FOR_ROOM = 100;
+	private static final int EXPECTED_AMOUNT_FOR_SERVICES = 0;
 	private static final int SERVICE_ID = 1;
-	private static final int CUSTOMER_ID = 3;
-	private static final int ROOM_ID = 2;
+	private static final int FIRST_CUSTOMER_ID = 3;
+	private static final int FIRST_ROOM_ID = 2;
 	private static final int NUMER_OF_ROWS = 5;
 	private static final String HOTEL_STATUS_TABLE_NAME = "hotelstatus";
-	TestDao testDao;
+	static TestDao testDao;
 	HotelStatusDAOImpl hotelStatusDao;
 	IDataSet loadedDataSet;
 
@@ -39,36 +42,39 @@ public class TestHotelStatusDAOImpl {
 
 	@Test
 	public void testBookRoom() throws SQLException {
-		hotelStatusDao.bookRoom(CUSTOMER_ID, ROOM_ID);
-		ResultSet hotelStatusRecords = hotelStatusDao.getHotelStatusRecordsForRoom(CUSTOMER_ID);
+		hotelStatusDao.bookRoom(FIRST_CUSTOMER_ID, FIRST_ROOM_ID);
+		ResultSet hotelStatusRecords = hotelStatusDao.getHotelStatusRecordsForRoom(FIRST_CUSTOMER_ID);
 		assertNotNull(hotelStatusRecords);
 		hotelStatusRecords.next();
-		assertEquals(CUSTOMER_ID, hotelStatusRecords.getInt("UserID"));
+		assertEquals(FIRST_CUSTOMER_ID, hotelStatusRecords.getInt("UserID"));
 	}
 
 	@Test
 	public void testBookAdditionalServices() throws SQLException {
-		hotelStatusDao.bookAdditionalServices(CUSTOMER_ID, new ArrayList<Integer>() {
+		bookService();
+		ResultSet usersAdditionalServices = hotelStatusDao.getUserAdditionalServices(FIRST_CUSTOMER_ID);
+		assertNotNull(usersAdditionalServices);
+		usersAdditionalServices.next();
+		assertEquals(FIRST_CUSTOMER_ID, usersAdditionalServices.getInt("UserID"));
+		assertEquals(20, usersAdditionalServices.getInt("Price"));
+
+	}
+	
+	private void bookService() throws SQLException {
+		hotelStatusDao.bookAdditionalServices(FIRST_CUSTOMER_ID, new ArrayList<Integer>() {
 			private static final long serialVersionUID = 1L;
 			{
 				add(SERVICE_ID);
 			}
 		});
-		
-		ResultSet usersAdditionalServices = hotelStatusDao.getUserAdditionalServices(CUSTOMER_ID);
-		assertNotNull(usersAdditionalServices);
-		usersAdditionalServices.next();
-		assertEquals(CUSTOMER_ID, usersAdditionalServices.getInt("UserID"));
-		assertEquals(20, usersAdditionalServices.getInt("Price"));
-
 	}
 	
 	@Test
-	public void testGetUserRoom() throws SQLException {
-		ResultSet userRoom = hotelStatusDao.getUserRoom(CUSTOMER_ID);
+	public void testGetUserRoom() throws Exception {
+		ResultSet userRoom = hotelStatusDao.getHotelStatusRecordsForRoom(SECOND_CUSTOMER_ID);
 		assertNotNull(userRoom);
 		userRoom.next();
-		assertEquals(ROOM_ID, userRoom.getInt("RoomNumber"));
+		assertEquals(SECOND_ROOM_ID, userRoom.getInt("RoomID"));
 	}
 	
 	@Test
@@ -80,19 +86,44 @@ public class TestHotelStatusDAOImpl {
 	
 	@Test
 	public void testGetCurrentTotalForServices() throws SQLException {
-		int totalForServices = hotelStatusDao.getCurrentTotalForServices(CUSTOMER_ID);
+		int totalForServices = hotelStatusDao.getCurrentTotalForServices(SECOND_CUSTOMER_ID);
 		assertEquals(EXPECTED_AMOUNT_FOR_SERVICES, totalForServices);
 	}
 	
 	@Test
 	public void getCurrentTotalForRoom() throws SQLException {
-		int totalForRoom = hotelStatusDao.getCurrentTotalForRoom(CUSTOMER_ID);
+		int totalForRoom = hotelStatusDao.getCurrentTotalForRoom(SECOND_CUSTOMER_ID);
 		assertEquals(EXPECTED_AMOUNT_FOR_ROOM, totalForRoom);
 	}
 	
+	@Test
+	public void testUpdateRoomEndDate() throws SQLException {
+		hotelStatusDao.updateRoomEndDate(FIRST_CUSTOMER_ID, FIRST_ROOM_ID);
+		ResultSet hotelStatusRecords = hotelStatusDao.getUserRoom(FIRST_CUSTOMER_ID);
+		assertNotNull(hotelStatusRecords);
+		LocalDate today = LocalDate.now();
+		hotelStatusRecords.next();
+		Timestamp endDate = hotelStatusRecords.getTimestamp("EndDate");
+		assertEquals(today, endDate.toLocalDateTime().toLocalDate());
+	}
 	
-//	@After
-//	public void tearDown() throws Exception {
-//		testDao.setUpDatabase();
-//	}
+	/**
+	 * Skontaj ovaj test bolje.
+	 * @throws SQLException
+	 */
+	@Test
+	public void testUpdateServiceEndDate() throws SQLException {
+		hotelStatusDao.updateServiceEndDate(FIRST_CUSTOMER_ID);
+		ResultSet result = hotelStatusDao.getHotelStatusRecordsForServices(FIRST_CUSTOMER_ID);
+		assertNotNull(result);
+		LocalDate today = LocalDate.now();
+		result.next();
+		Timestamp endDate = result.getTimestamp("EndDate");
+		assertEquals(today, endDate.toLocalDateTime().toLocalDate());
+	}
+	
+	@AfterClass
+	public static void tearDown() throws Exception {
+		testDao.setUpDatabase();
+	}
 }
